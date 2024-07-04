b534f58dd7659745f553f43957eb387027851981
zengdihong <996487756@qq.com>
2024-07-04 15:50:12 +0800

    optimize import

--- src/GroupAnagrams.java
+++ src/GroupAnagrams.java
@@ -1,4 +1,7 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class GroupAnagrams {
     public List<List<String>> groupAnagrams(String[] strs) {
