f45a3397da15bd427489f43889c512359e55f3b4
zengdihong <996487756@qq.com>
2024-07-05 17:25:01 +0800

    Permute

--- src/MainFunc.java
+++ src/MainFunc.java
@@ -25,8 +25,10 @@ public class MainFunc {
 //        var maxSlidingWindow = MaxSlidingWindow.maxSlidingWindow(new int[]{1, 3, -1, -3, 5, 3, 6, 7}, 3);
 
 //        System.out.println(Arrays.toString(maxSlidingWindow));
-        int len= LongestConsecutive.longestConsecutive(new int[]{0, 3, 7, 2, 5, 8, 4, 6, 0, 1});
-        System.out.println(len);
+//        int len= LongestConsecutive.longestConsecutive(new int[]{0, 3, 7, 2, 5, 8, 4, 6, 0, 1});
+//        System.out.println(len);
+        Permute permute = new Permute();
+        var res = permute.permute(new int[]{1, 2, 3});
         System.out.println("test create branch ");
     }
 }
